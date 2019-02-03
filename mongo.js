50d7f6377a512a7da6b3df97b49d3e598ab9f164
Lionking93 <leo.kallonen@gmail.com>
2019-02-03 18:40:40 +0200

    osa 3 t3.12

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3){
+    console.log("Give password as an argument!")
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb://fullstack:${password}@ds135974.mlab.com:35974/fullstack_puhelinluettelo`
+
+mongoose.connect(url, { useNewUrlParser: true})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+    const newName = process.argv[3]
+    const newNumber = process.argv[4]
+
+    const person = new Person({
+        name: newName,
+        number: newNumber
+    })
+
+    person.save().then(response => {
+        console.log(`lisätään ${newName} numero ${newNumber} luetteloon`)
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        console.log('puhelinluettelo:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    console.log('suljetaan yhteys')
+    mongoose.connection.close();
+}
