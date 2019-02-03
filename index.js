518c4b542d86e63ac26fa52637c5337cf1e86fbe
Lionking93 <leo.kallonen@gmail.com>
2019-02-03 12:19:16 +0200

    osa 3 t3.1

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("Hello there!")
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "045-1236543"
+    },
+    {
+        id: 2,
+        name: "Arto Järvinen",
+        number: "041-21423123"
+    },
+    {
+        id: 3,
+        name: "Lea Kutvonen",
+        number: "040-4323234"
+    },
+    {
+        id: 4,
+        name: "Martti Tienari",
+        number: "09-784232"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
 
-const port = 3000
+const port = 3001
 app.listen(port, () => {
     console.log("Listening for requests to port", port)
 })
