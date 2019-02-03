730f2ba2df3ea8b5e3b057dd2fc3dd1733902626
Lionking93 <leo.kallonen@gmail.com>
2019-02-03 14:13:10 +0200

    osa 3 t3.7

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const MAX_ID = 100000
 
 app.use(bodyParser.json())
+
+app.use(morgan('tiny'))
 
 let persons = [
     {
